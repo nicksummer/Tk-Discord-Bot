c7979b81937b41050e60f4b83eaa051a7069d098
nicksummer <52978388+nicksummer@users.noreply.github.com>
2021-05-13 00:43:33 -0400

    Delete urgay.js

--- src/commands/urgay.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (msg) => {
-	let helpMsg = "I'm gay \n";;
-    let url = "https://tenor.com/view/suck-cock-suckcock-bj-blowjob-gif-14925832";
-    msg.channel.send(url);
-    await msg.channel.send(helpMsg);
-}
